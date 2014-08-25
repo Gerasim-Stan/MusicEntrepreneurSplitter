7fc74fb0dd969fb286996fee6f1ef96725cfc201
Gerasim-Stan <gerasim.stanchev@gmail.com>
2014-08-25 14:32:44 +0300

    Classes renewing

--- src/pymplayer.py
+++ src/pymplayer.py
@@ -1,6 +1,8 @@
 from os import path
 from pygame import mixer
 from pydub import AudioSegment
+import numpy as np
+import subprocess as sp
 
 class MusicPlayer:
     main_mixer = mixer
@@ -65,10 +67,10 @@ class MusicPlayer:
             print("Unable to set volume")
 
     def _get_volume(self):
-        return MusicPlayer.main_mixer.get_volume()
+        return MusicPlayer.main_mixer.music.get_volume()
 
     def _get_cond(self):
-        return MusicPlayer.main_mixer.get_busy()
+        return MusicPlayer.main_mixer.music.get_busy()
 
     def _rewind(self):
         try:
@@ -92,13 +94,13 @@ class MusicPlayer:
             print("Can't add the file to queue")
 
 
-    def _cut(self, starting_p, ending_p):
+    def _cut(self, starting_p, ending_p, filePathAndName):
         if MusicPlayer.sound_path == "":
             print("Unable to cut")
             return False
         sound = AudioSegment.from_mp3(MusicPlayer.sound_path)
         splitted_sound = sound[starting_p * 1000:ending_p * 1000]
-        splitted_sound.export(MusicPlayer.sound_path[:-4] + " (Splitted).mp3")
+        splitted_sound.export(filePathAndName + " (Splitted).mp3")
 
     def _concat(self, added_sound_path, side = "ending"):
         if MusicPlayer.sound_path == "" or added_sound_path == "":
@@ -111,3 +113,17 @@ class MusicPlayer:
         else:
             final_sound = sound2 + sound1
         final_sound.export(MusicPlayer.sound_path[:-4] + " (Concatenated).mp3")
+
+    def _sound_to_array(self, filePath = sound_path):
+        command = [ "ffmpeg", '-i', filePath, '-f',
+        's16le', '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', '-']
+        pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
+        raw_audio = pipe.stdout.read(88200*4)
+        audio_array = np.fromstring(raw_audio, dtype="int16")
+        audio_array = audio_array.reshape((len(audio_array)/2,2))
+
+        return audio_array
+
+
+
+
