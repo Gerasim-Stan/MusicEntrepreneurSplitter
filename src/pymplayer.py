ba18c5d9c9789e0488cc72875aa248ba8ddd355b
Gerasim-Stan <gerasim.stanchev@gmail.com>
2014-09-05 10:27:45 +0300

    Add changes to player

--- src/pymplayer.py
+++ src/pymplayer.py
@@ -4,126 +4,71 @@ from pydub import AudioSegment
 import numpy as np
 import subprocess as sp
 
+
 class MusicPlayer:
     main_mixer = mixer
     sound_path = ""
+
     def __init__(self):
         MusicPlayer.main_mixer.init()
 
-    def _import_file(self, file_path):
+    def importFile(self, file_path):
         if(path.isfile(file_path) and file_path[-4:] == ".mp3"):
             MusicPlayer.sound_path = file_path
             MusicPlayer.main_mixer.music.load(file_path)
-            print("An mp3 file is added.")
-            return True
-        else:
-            print("This is not an mp3 file!")
-            return False
+            return
 
-    def _play(self):
+    def play(self):
         try:
             MusicPlayer.main_mixer.music.stop()
         except:
             pass
-        try:
-            MusicPlayer.main_mixer.music.play()
-            if MusicPlayer.main_mixer.music.get_busy() == 1:
-                return True
-        except:
-            print("Unable to play")
-            return False
+        MusicPlayer.main_mixer.music.play()
 
-    def _pause(self):
-        try:
-            MusicPlayer.main_mixer.music.pause()
-            if MusicPlayer.main_mixer.music.get_busy() == 1:
-                return True
-        except:
-            print("Unable to pause")
-            return False
+    def pause(self):
+        MusicPlayer.main_mixer.music.pause()
 
-    def _stop(self):
-        try:
-            MusicPlayer.main_mixer.music.stop()
-        except:
-            print("Unable to stop")
+    def stop(self):
+        MusicPlayer.main_mixer.music.stop()
 
-    def _unpause(self):
-        try:
-            MusicPlayer.main_mixer.music.unpause()
-        except:
-            print("Unable to unpause")
+    def unpause(self):
+        MusicPlayer.main_mixer.music.unpause()
 
-    def _play_position(self, position):
-        try:
-            MusicPlayer.main_mixer.music.set_pos(position)
-        except:
-            print("Unable to position correctly")
+    def setPlayPosition(self, position):
+        MusicPlayer.main_mixer.music.set_pos(position)
 
-    def _set_volume(self, value):
-        try:
-            MusicPlayer.main_mixer.music.set_volume(value)
-        except:
-            print("Unable to set volume")
+    def setVolume(self, value):
+        MusicPlayer.main_mixer.music.set_volume(value)
 
-    def _get_volume(self):
-        return MusicPlayer.main_mixer.music.get_volume()
-
-    def _get_cond(self):
+    def getCond(self):
         return MusicPlayer.main_mixer.music.get_busy()
 
-    def _rewind(self):
-        try:
-            MusicPlayer.main_mixer.music.rewind()
-        except:
-            print("Unable to rewind")
-
-    def _fadeout(self, time):
-        try:
-            MusicPlayer.main_mixer.music.fadeout(time * 1000)
-        except:
-            print("Unable to fadeout")
-
-    def _queue(self, file_path):
-        if(path.isfile(file_path) and file_path[-4:] == ".mp3"):
-            try:
-                MusicPlayer.main_mixer.music.queue(file_path)
-            except:
-                print("Can't add the file to queue")
-        else:
-            print("Can't add the file to queue")
-
-
-    def _cut(self, starting_p, ending_p, filePathAndName):
-        if MusicPlayer.sound_path == "":
-            print("Unable to cut")
-            return False
+    def getLength(self):
         sound = AudioSegment.from_mp3(MusicPlayer.sound_path)
-        splitted_sound = sound[starting_p * 1000:ending_p * 1000]
+        return sound.duration_seconds
+
+    def cut(self, starting_p, ending_p, filePathAndName,
+            startLulling, endLulling):
+        sound = AudioSegment.from_mp3(MusicPlayer.sound_path)
+        splitted_sound = sound[starting_p*1000:ending_p * 1000]
+        if startLulling:
+            splitted_sound = splitted_sound.fade_in(4000)
+        if endLulling:
+            splitted_sound = splitted_sound.fade_out(4000)
         splitted_sound.export(filePathAndName + " (Splitted).mp3")
 
-    def _concat(self, added_sound_path, side = "ending"):
-        if MusicPlayer.sound_path == "" or added_sound_path == "":
-            print("Unable to concatenate")
-            return False
-        sound1 = AudioSegment.from_mp3(MusicPlayer.sound_path)
-        sound2 = AudioSegment.from_mp3(added_sound_path)
-        if side == "right":
-            final_sound = sound1 + sound2
-        else:
-            final_sound = sound2 + sound1
-        final_sound.export(MusicPlayer.sound_path[:-4] + " (Concatenated).mp3")
+    def concat(self, leftTune, rightTune, songWritePath):
+        sound1 = AudioSegment.from_mp3(leftTune)
+        sound2 = AudioSegment.from_mp3(rightTune)
+        final_sound = sound1 + sound2
+        final_sound.export(songWritePath + " (Concatenated).mp3")
 
-    def _sound_to_array(self, filePath = sound_path):
-        command = [ "ffmpeg", '-i', filePath, '-f',
-        's16le', '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', '-']
+    def soundToArray(self, filePath=sound_path):
+        command = ["ffmpeg", '-i', filePath, '-f', 's16le', '-acodec',
+                   'pcm_s16le', '-ar', '44100', '-ac', '2', '-']
         pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
         raw_audio = pipe.stdout.read(88200*4)
         audio_array = np.fromstring(raw_audio, dtype="int16")
-        audio_array = audio_array.reshape((len(audio_array)/2,2))
+        audio_array = audio_array.reshape((len(audio_array)/2, 2))
 
         return audio_array
-
-
-
-
