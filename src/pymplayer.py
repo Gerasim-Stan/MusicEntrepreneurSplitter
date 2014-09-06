34d66e8b67ba65791cddebf84dd004b1d78290be
Gerasim-Stan <gerasim.stanchev@gmail.com>
2014-09-07 01:06:59 +0300

    Revised variable names, and added comments

--- src/pymplayer.py
+++ src/pymplayer.py
@@ -6,69 +6,85 @@ import subprocess as sp
 
 
 class MusicPlayer:
-    main_mixer = mixer
-    sound_path = ""
+    """Defines and implements functions for control of music player,
+       and editing music.
+    """
+    mainMixer = mixer
+    soundPath = ""
 
     def __init__(self):
-        MusicPlayer.main_mixer.init()
+        MusicPlayer.mainMixer.init()
 
-    def importFile(self, file_path):
-        if(path.isfile(file_path) and file_path[-4:] == ".mp3"):
-            MusicPlayer.sound_path = file_path
-            MusicPlayer.main_mixer.music.load(file_path)
+    def importFile(self, filePath):
+        """Provides tools for importing of file"""
+        if(path.isfile(filePath) and filePath[-4:] == ".mp3"):
+            MusicPlayer.soundPath = filePath
+            MusicPlayer.mainMixer.music.load(filePath)
             return
 
     def play(self):
+        """Plays loaded tune"""
         try:
-            MusicPlayer.main_mixer.music.stop()
+            MusicPlayer.mainMixer.music.stop()
         except:
             pass
-        MusicPlayer.main_mixer.music.play()
+        MusicPlayer.mainMixer.music.play()
 
     def pause(self):
-        MusicPlayer.main_mixer.music.pause()
+        """Pauses playing tune"""
+        MusicPlayer.mainMixer.music.pause()
 
     def stop(self):
-        MusicPlayer.main_mixer.music.stop()
+        """Stops playing tune"""
+        MusicPlayer.mainMixer.music.stop()
 
     def unpause(self):
-        MusicPlayer.main_mixer.music.unpause()
+        """Unpauses paused tune"""
+        MusicPlayer.mainMixer.music.unpause()
 
     def setPlayPosition(self, position):
-        MusicPlayer.main_mixer.music.set_pos(position)
+        """Sets position for the tune to start"""
+        MusicPlayer.mainMixer.music.set_pos(position)
 
     def setVolume(self, value):
-        MusicPlayer.main_mixer.music.set_volume(value)
+        """Sets volume in range [0, 1]"""
+        MusicPlayer.mainMixer.music.set_volume(value)
 
     def getCond(self):
-        return MusicPlayer.main_mixer.music.get_busy()
+        """Gets current condition of the player"""
+        return MusicPlayer.mainMixer.music.get_busy()
 
     def getLength(self):
-        sound = AudioSegment.from_mp3(MusicPlayer.sound_path)
+        """Gets length of the current loaded tune"""
+        sound = AudioSegment.from_mp3(MusicPlayer.soundPath)
         return sound.duration_seconds
 
-    def cut(self, starting_p, ending_p, filePathAndName,
+    def cut(self, startingPos, endingPos, filePathAndName,
             startLulling, endLulling):
-        sound = AudioSegment.from_mp3(MusicPlayer.sound_path)
-        splitted_sound = sound[starting_p*1000:ending_p * 1000]
+        """Cuts mp3 file in range [starting Pos, endingPos],
+           and eventually fades in the beginning or the end"""
+        sound = AudioSegment.from_mp3(MusicPlayer.soundPath)
+        splittedSound = sound[startingPos*1000:endingPos * 1000]
         if startLulling:
-            splitted_sound = splitted_sound.fade_in(4000)
+            splittedSound = splittedSound.fade_in(4000)
         if endLulling:
-            splitted_sound = splitted_sound.fade_out(4000)
-        splitted_sound.export(filePathAndName + " (Splitted).mp3")
+            splittedSound = splittedSound.fade_out(4000)
+        splittedSound.export(filePathAndName + " (Splitted).mp3")
 
     def concat(self, leftTune, rightTune, songWritePath):
+        """Concatenates two tunes, and writes the result at given directory"""
         sound1 = AudioSegment.from_mp3(leftTune)
         sound2 = AudioSegment.from_mp3(rightTune)
-        final_sound = sound1 + sound2
-        final_sound.export(songWritePath + " (Concatenated).mp3")
+        finalSound = sound1 + sound2
+        finalSound.export(songWritePath + " (Concatenated).mp3")
 
-    def soundToArray(self, filePath=sound_path):
+    def soundToArray(self, filePath=soundPath):
+        """Converts the loaded mp3 file to numpy array"""
         command = ["ffmpeg", '-i', filePath, '-f', 's16le', '-acodec',
                    'pcm_s16le', '-ar', '44100', '-ac', '2', '-']
         pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
-        raw_audio = pipe.stdout.read(88200*4)
-        audio_array = np.fromstring(raw_audio, dtype="int16")
-        audio_array = audio_array.reshape((len(audio_array)/2, 2))
+        rawAudio = pipe.stdout.read(88200*4)
+        audioArray = np.fromstring(rawAudio, dtype="int16")
+        audioArray = audioArray.reshape((len(audioArray)/2, 2))
 
-        return audio_array
+        return audioArray
